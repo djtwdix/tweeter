cd6d448f1df67d314c6643370de0213d13127ebf
djtwdix <djtwd.ix@icloud.com>
2021-02-23 13:37:52 -0400

    Remove console logs from testing character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,7 @@ $(document).ready(() => {
   $("#tweet-text").on("input", function () {
     characters = $(this).val().length;
     charactersLeft = 140 - characters
-    console.log(charactersLeft)
     $(".counter").text(charactersLeft);
-    console.log("parent:", $(this).parent())
     const parent = $(this).parent();
     if (charactersLeft < 0) {
       $(parent).find(".counter").css({ "color": "red" });
