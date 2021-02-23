3a1c0b853e3bcc96f1a793fabb69c82762738333
djtwdix <djtwd.ix@icloud.com>
2021-02-23 13:37:23 -0400

    Implement character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
-$(document).ready( () => {
+$(document).ready(() => {
   console.log("This is a test");
+  $("#tweet-text").on("input", function () {
+    characters = $(this).val().length;
+    charactersLeft = 140 - characters
+    console.log(charactersLeft)
+    $(".counter").text(charactersLeft);
+    console.log("parent:", $(this).parent())
+    const parent = $(this).parent();
+    if (charactersLeft < 0) {
+      $(parent).find(".counter").css({ "color": "red" });
+    } else {
+      $(parent).find(".counter").css({ "color": "#545149" });
+    }
+  })
 })
+
