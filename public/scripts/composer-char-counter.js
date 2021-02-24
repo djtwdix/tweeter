e6c77cb0556bc400d3a47c25b0dead8cabd1eea4
djtwdix <djtwd.ix@icloud.com>
2021-02-24 09:04:38 -0400

    Add descriptive comment to composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,16 @@
+/**
+ * event listener on input of text area in new tweet form
+ * charCounter() captures amount of characters typed
+ * counter element on page displays difference between max characters and characters typed
+ * counter element displays red if user has gone over 140 characters
+ */
+
 $(document).ready(() => {
-  console.log("This is a test");
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function charCounter() {
     characters = $(this).val().length;
     charactersLeft = 140 - characters
-    $(".counter").text(charactersLeft);
     const parent = $(this).parent();
+    $(parent).find(".counter").text(charactersLeft);
     if (charactersLeft < 0) {
       $(parent).find(".counter").css({ "color": "red" });
     } else {
