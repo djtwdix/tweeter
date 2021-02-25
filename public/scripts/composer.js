00373870ba6a359a8e5683f2936b4345e48f2362
djtwdix <djtwd.ix@icloud.com>
2021-02-25 15:59:05 -0400

    Move character remaining logic back into event listener

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,41 +1,7 @@
 /**
- * event listener on input of text area in new tweet form
- * charCounter() captures amount of characters typed
- * counter element on page displays difference between max characters and characters typed
- * counter element displays red if user has gone over 140 characters
+ * showArrowOnScroll() fades scroll up arrow in or out
+ * depending on current scroll top postion
  */
-
-$(document).ready(() => {
-
-  //count characters in tweet form and display remaining characters
-
-  $("#tweet-text").on("input", function () {
-    characterCount()
-  });
-
-  //toggle tweet form on click of write new tweet
-
-  $("#tweet-reveal").on("click", function () {
-    $("#tweet-form").slideToggle("slow");
-    $("#tweet-text").focus();
-    $("#input-error").slideUp("slow");
-  });
-
-  $(document).scroll(function () {
-    showArrowOnScroll();
-  });;
-
-  $('#scroll-up-arrow').on("click", function () {
-    $("#tweet-text").focus();
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    $("#tweet-form").slideDown("slow");
-    $("#input-error").slideUp("slow");
-  });
-
-  
-});
-
 
 const showArrowOnScroll = () => {
   var y = $(this).scrollTop();
@@ -48,14 +14,64 @@ const showArrowOnScroll = () => {
   }
 }
 
-const characterCount = () => {
-  characters = $(this).val().length;
-  charactersLeft = 140 - characters
-  const parent = $(this).parent();
-  $(parent).find(".counter").text(charactersLeft);
-  if (charactersLeft < 0) {
-    $(parent).find(".counter").css({ "color": "rgb(255, 57, 57)" });
-  } else {
-    $(parent).find(".counter").css({ "color": "#545149" });
-  }
+/**
+ * scrollUp() scrolls to top of page
+ * reveals tweet form with focus
+ * hides error message if present
+ */
+
+const scrollUp = () => {
+  $("#tweet-text").focus();
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  $("#tweet-form").slideDown("slow");
+  $("#input-error").slideUp("slow");
 }
+
+/**
+ * scrollUp() toggles tweet form with focus
+ * hides error message if present
+ */
+
+const tweetToggle = () => {
+  $("#tweet-form").slideToggle("slow");
+  $("#tweet-text").focus();
+  $("#input-error").slideUp("slow");
+}
+
+$(document).ready(() => {
+
+  /**
+ * listens on input and registers length
+ * displays remaining characters on page
+ * turns red if limit exceeded
+ */
+
+  $("#tweet-text").on("input", function () {
+    characters = $(this).val().length;
+    charactersLeft = 140 - characters
+    const parent = $(this).parent();
+    $(parent).find(".counter").text(charactersLeft);
+    if (charactersLeft < 0) {
+      $(parent).find(".counter").css({ "color": "rgb(255, 57, 57)" });
+    } else {
+      $(parent).find(".counter").css({ "color": "#545149" });
+    }
+  });
+
+
+  $("#tweet-reveal").on("click", function () {
+   tweetToggle();
+  });
+
+  $(document).scroll(function () {
+    showArrowOnScroll();
+  });;
+
+  $('#scroll-up-arrow').on("click", function () {
+    scrollUp();
+  });
+
+  
+});
+
